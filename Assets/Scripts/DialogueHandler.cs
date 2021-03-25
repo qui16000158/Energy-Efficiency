b8d92093d33a43f868a338b946048c15cf84a107
qui16000158 <55698561+qui16000158@users.noreply.github.com>
2021-03-25 15:03:33 +0000

    Minor issues

--- Assets/Scripts/DialogueHandler.cs
+++ Assets/Scripts/DialogueHandler.cs
@@ -65,7 +65,7 @@ public class DialogueHandler : MonoBehaviour
             textBox.text += input[currentIndex];
 
             currentIndex++;
-            yield return new WaitForSeconds(0.05f);
+            yield return new WaitForSeconds(1/60.0f);
         }
     }
 }
