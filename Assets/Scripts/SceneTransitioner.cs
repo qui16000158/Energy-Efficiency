56c37a6b3592401f0b41afc66904ebb373221079
qui16000158 <55698561+qui16000158@users.noreply.github.com>
2021-03-31 14:19:48 +0100

    Added Main Menu

---
+++ Assets/Scripts/SceneTransitioner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitioner : MonoBehaviour
+{
+    public void Load(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
